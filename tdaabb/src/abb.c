b1a55b3d1637c75c1d2c3be6bcd63dc86a582512
LuchoLorenzo1 <lucianolorenzo1@hotmail.com>
2022-05-07 20:26:53 -0300

    recorridos

--- tdaabb/src/abb.c
+++ tdaabb/src/abb.c
@@ -17,22 +17,57 @@ abb_t *abb_crear(abb_comparador comparador)
 	return arbol;
 }
 
+nodo_abb_t *insertar_recursivo(nodo_abb_t *nodo, void *elemento,
+			       abb_comparador comparador)
+{
+	if (!nodo) {
+		nodo_abb_t *nuevo_nodo = malloc(sizeof(nodo_abb_t));
+		if (!nuevo_nodo)
+			return NULL;
+		nuevo_nodo->elemento = elemento;
+		nuevo_nodo->derecha = NULL;
+		nuevo_nodo->izquierda = NULL;
+		return nuevo_nodo;
+	}
+	int n = comparador(elemento, nodo->elemento);
+	if (n <= 0) {
+		nodo_abb_t *nuevo_nodo = insertar_recursivo(
+			nodo->izquierda, elemento, comparador);
+		if (!nuevo_nodo)
+			return NULL;
+		nodo->izquierda = nuevo_nodo;
+	} else {
+		nodo_abb_t *nuevo_nodo = insertar_recursivo(
+			nodo->izquierda, elemento, comparador);
+		if (!nuevo_nodo)
+			return NULL;
+		nodo->derecha = nuevo_nodo;
+	}
+	return nodo;
+}
+
 abb_t *abb_insertar(abb_t *arbol, void *elemento)
 {
-	if (!arbol || !arbol->nodo_raiz)
+	if (!arbol)
 		return NULL;
 
-	nodo_abb_t *nodo_actual = arbol->nodo_raiz;
-
-	int n = arbol->comparador(elemento, nodo_actual->elemento);
-
-	if(n<=0){
-		nodo_actual = nodo_actual->izquierda;
-	} else {
-		nodo_actual = nodo_actual->derecha;
+	if (!arbol->nodo_raiz) {
+		nodo_abb_t *raiz = malloc(sizeof(nodo_abb_t));
+		if (!raiz)
+			return NULL;
+		raiz->derecha = NULL;
+		raiz->izquierda = NULL;
+		raiz->elemento = elemento;
+		arbol->nodo_raiz = raiz;
+		arbol->tamanio++;
+		return arbol;
 	}
 
-
+	nodo_abb_t *raiz = insertar_recursivo(arbol->nodo_raiz, elemento,
+					      arbol->comparador);
+	if (!raiz)
+		return NULL;
+	arbol->tamanio++;
 	return arbol;
 }
 
@@ -48,14 +83,14 @@ void *abb_buscar(abb_t *arbol, void *elemento)
 
 bool abb_vacio(abb_t *arbol)
 {
-	if(!arbol || !arbol->nodo_raiz)
+	if (!arbol || !arbol->nodo_raiz)
 		return true;
 	return false;
 }
 
 size_t abb_tamanio(abb_t *arbol)
 {
-	if(!arbol)
+	if (!arbol)
 		return 0;
 	return arbol->tamanio;
 }
@@ -68,14 +103,89 @@ void abb_destruir_todo(abb_t *arbol, void (*destructor)(void *))
 {
 }
 
+size_t recorrer_inorden(nodo_abb_t *nodo, bool (*funcion)(void *, void *),
+			void *aux, bool *continuar)
+{
+	if (!nodo)
+		return 0;
+
+	size_t izq = recorrer_inorden(nodo->izquierda, funcion, aux, continuar);
+	if (!*continuar)
+		return izq;
+
+	*continuar = funcion(nodo->elemento, aux);
+	if (!*continuar)
+		return izq + 1;
+
+	size_t der = recorrer_inorden(nodo->derecha, funcion, aux, continuar);
+	return izq + der + 1;
+}
+
+size_t recorrer_preorden(nodo_abb_t *nodo, bool (*funcion)(void *, void *),
+			 void *aux, bool *continuar)
+{
+	if (!nodo)
+		return 0;
+
+	*continuar = funcion(nodo->elemento, aux);
+	if (!*continuar)
+		return 1;
+
+	size_t izq = recorrer_inorden(nodo->izquierda, funcion, aux, continuar);
+	if (!*continuar)
+		return izq + 1;
+
+	size_t der = recorrer_inorden(nodo->derecha, funcion, aux, continuar);
+	return izq + der + 1;
+}
+
+size_t recorrer_postorden(nodo_abb_t *nodo, bool (*funcion)(void *, void *),
+			  void *aux, bool *continuar)
+{
+	if (!nodo)
+		return 0;
+
+	size_t izq = recorrer_inorden(nodo->izquierda, funcion, aux, continuar);
+	if (!*continuar)
+		return izq;
+
+	size_t der = recorrer_inorden(nodo->derecha, funcion, aux, continuar);
+	if (!*continuar)
+		return izq + der;
+
+	*continuar = funcion(nodo->elemento, aux);
+	return izq + der + 1;
+}
+
 size_t abb_con_cada_elemento(abb_t *arbol, abb_recorrido recorrido,
 			     bool (*funcion)(void *, void *), void *aux)
 {
-	return 0;
+	bool t = true;
+	switch (recorrido) {
+	case INORDEN:
+		return recorrer_inorden(arbol->nodo_raiz, funcion, aux, &t);
+	case PREORDEN:
+		return recorrer_preorden(arbol->nodo_raiz, funcion, aux, &t);
+	case POSTORDEN:
+		return recorrer_postorden(arbol->nodo_raiz, funcion, aux, &t);
+	default:
+		return 0;
+	}
 }
 
 size_t abb_recorrer(abb_t *arbol, abb_recorrido recorrido, void **array,
 		    size_t tamanio_array)
 {
+	switch (recorrido) {
+	case INORDEN:
+		break;
+	case PREORDEN:
+		break;
+	case POSTORDEN:
+		break;
+	default:
+		break;
+	}
+
 	return 0;
 }
