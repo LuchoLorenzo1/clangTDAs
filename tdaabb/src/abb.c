436e7777d8fbb14da1979d4f78a849b2fd4dd5af
LuchoLorenzo1 <lucianolorenzo1@hotmail.com>
2022-05-09 12:10:32 -0300

    lol

--- tdaabb/src/abb.c
+++ tdaabb/src/abb.c
@@ -1,6 +1,7 @@
 #include "abb.h"
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 abb_t *abb_crear(abb_comparador comparador)
 {
@@ -36,8 +37,8 @@ nodo_abb_t *insertar_recursivo(nodo_abb_t *nodo, void *elemento,
 			return NULL;
 		nodo->izquierda = nuevo_nodo;
 	} else {
-		nodo_abb_t *nuevo_nodo = insertar_recursivo(
-			nodo->derecha, elemento, comparador);
+		nodo_abb_t *nuevo_nodo =
+			insertar_recursivo(nodo->derecha, elemento, comparador);
 		if (!nuevo_nodo)
 			return NULL;
 		nodo->derecha = nuevo_nodo;
@@ -47,42 +48,104 @@ nodo_abb_t *insertar_recursivo(nodo_abb_t *nodo, void *elemento,
 
 abb_t *abb_insertar(abb_t *arbol, void *elemento)
 {
-	if (!arbol)
+	if (!arbol || !elemento)
 		return NULL;
-
-	if (!arbol->nodo_raiz) {
-		nodo_abb_t *raiz = malloc(sizeof(nodo_abb_t));
-		if (!raiz)
-			return NULL;
-		raiz->derecha = NULL;
-		raiz->izquierda = NULL;
-		raiz->elemento = elemento;
-		arbol->nodo_raiz = raiz;
-		arbol->tamanio++;
-		return arbol;
-	}
 
 	nodo_abb_t *raiz = insertar_recursivo(arbol->nodo_raiz, elemento,
 					      arbol->comparador);
+
 	if (!raiz)
 		return NULL;
+
+	if (!arbol->nodo_raiz)
+		arbol->nodo_raiz = raiz;
+
 	arbol->tamanio++;
 	return arbol;
 }
 
-void *abb_quitar(abb_t *arbol, void *elemento)
+nodo_abb_t *predecesor_inmediato(nodo_abb_t *nodo)
 {
-	return NULL;
+	nodo_abb_t *act = nodo;
+	nodo_abb_t *ant = NULL;
+	while (act->derecha) {
+		ant = act;
+		act = act->derecha;
+	}
+	if (ant)
+		ant->derecha = NULL;
+	return act;
 }
 
-void *buscar_recursivo(nodo_abb_t *nodo, void *elemento, abb_comparador comparador)
+void *quitar_recursivo(nodo_abb_t *nodo, void *elemento,
+		       abb_comparador comparador, void **extraido)
 {
-	if(!nodo)
+	if (!nodo)
 		return NULL;
-	int n = comparador(elemento, nodo->elemento);
-	if (n == 0)
+
+	int comparacion = comparador(elemento, nodo->elemento);
+	if (comparacion == 0) {
+		nodo_abb_t *izq = nodo->izquierda;
+		nodo_abb_t *der = nodo->derecha;
+		*extraido = nodo->elemento;
+		free(nodo);
+		nodo = NULL;
+
+		if (izq && der) {
+			nodo_abb_t *reemplazo;
+			if (izq->derecha) {
+				reemplazo = predecesor_inmediato(izq);
+				nodo_abb_t *todo_izquierda = reemplazo;
+				while (todo_izquierda->izquierda)
+					todo_izquierda =
+						todo_izquierda->izquierda;
+				todo_izquierda->izquierda = izq;
+			} else {
+				reemplazo = izq;
+			}
+			reemplazo->derecha = der;
+			return reemplazo;
+		}
+		if (!izq && der)
+			return der;
+		if (izq && !der)
+			return izq;
+		if (!izq && !der)
+			return NULL;
+
+	} else if (comparacion < 0) {
+		nodo->izquierda = quitar_recursivo(nodo->izquierda, elemento,
+						   comparador, extraido);
+	} else {
+		nodo->derecha = quitar_recursivo(nodo->derecha, elemento,
+						 comparador, extraido);
+	}
+	return nodo;
+}
+
+void *abb_quitar(abb_t *arbol, void *elemento)
+{
+	if (!arbol || !elemento)
+		return NULL;
+
+	void *extraido = NULL;
+	arbol->nodo_raiz = quitar_recursivo(arbol->nodo_raiz, elemento,
+					    arbol->comparador, &extraido);
+	if (!extraido)
+		return NULL;
+	arbol->tamanio--;
+	return extraido;
+}
+
+void *buscar_recursivo(nodo_abb_t *nodo, void *elemento,
+		       abb_comparador comparador)
+{
+	if (!nodo)
+		return NULL;
+	int comparacion = comparador(elemento, nodo->elemento);
+	if (comparacion == 0)
 		return nodo->elemento;
-	else if (n < 0)
+	else if (comparacion < 0)
 		return buscar_recursivo(nodo->izquierda, elemento, comparador);
 	else
 		return buscar_recursivo(nodo->derecha, elemento, comparador);
@@ -90,7 +153,7 @@ void *buscar_recursivo(nodo_abb_t *nodo, void *elemento, abb_comparador comparad
 
 void *abb_buscar(abb_t *arbol, void *elemento)
 {
-	if(!arbol || !elemento)
+	if (!arbol || !elemento)
 		return NULL;
 	return buscar_recursivo(arbol->nodo_raiz, elemento, arbol->comparador);
 }
@@ -111,18 +174,18 @@ size_t abb_tamanio(abb_t *arbol)
 
 void destruir_recursivo(nodo_abb_t *nodo, void (*destructor)(void *))
 {
-	if(!nodo)
+	if (!nodo)
 		return;
 	destruir_recursivo(nodo->izquierda, destructor);
 	destruir_recursivo(nodo->derecha, destructor);
-	if(destructor)
+	if (destructor)
 		destructor(nodo->elemento);
 	free(nodo);
 }
 
 void abb_destruir_todo(abb_t *arbol, void (*destructor)(void *))
 {
-	if(!arbol)
+	if (!arbol)
 		return;
 	destruir_recursivo(arbol->nodo_raiz, destructor);
 	free(arbol);
@@ -130,7 +193,7 @@ void abb_destruir_todo(abb_t *arbol, void (*destructor)(void *))
 
 void abb_destruir(abb_t *arbol)
 {
-	if(!arbol)
+	if (!arbol)
 		return;
 	destruir_recursivo(arbol->nodo_raiz, NULL);
 	free(arbol);
@@ -213,33 +276,38 @@ size_t abb_con_cada_elemento(abb_t *arbol, abb_recorrido recorrido,
 	}
 }
 
-/* typedef struct array_con_tamanio { */
-/* 	void **array; */
-/* 	size_t max; */
-/* 	size_t act; */
-/* } arr_t; */
+typedef struct array_con_tamanio {
+	void ***puntero_doble_array;
+	size_t max;
+	size_t *act;
+} arr_t;
 
-/* bool insertar_array(void *elemento, void *array_con_tamanio){ */
-/* 	arr_t *array = (arr_t*)array_con_tamanio; */
+bool insertar_array(void *elemento, void *array_con_tamanio)
+{
+	arr_t aux = *(arr_t *)array_con_tamanio;
 
-/* 	array->array[array->act] = elemento; */
-/* 	array->act++; */
+	void **array = *aux.puntero_doble_array;
 
-/* 	if(array->act == array->max) */
-/* 		return false; */
-/* 	return true; */
-/* } */
+	array[*aux.act] = elemento;
+	(*aux.act)++;
 
-/* size_t abb_recorrer(abb_t *arbol, abb_recorrido recorrido, void **array, */
-/* 		    size_t tamanio_array) */
-/* { */
-/* 	if (!arbol || !array || tamanio_array <= 0) */
-/* 		return 0; */
+	if (*aux.act == aux.max)
+		return false;
+	return true;
+}
 
-/* 	arr_t *aux = &array; */
-/* 	aux->array = array; */
-/* 	aux->max = tamanio_array; */
-/* 	aux->act = 0; */
+size_t abb_recorrer(abb_t *arbol, abb_recorrido recorrido, void **array,
+		    size_t tamanio_array)
+{
+	if (!arbol || !array || tamanio_array <= 0)
+		return 0;
 
-/* 	return abb_con_cada_elemento(arbol, recorrido, insertar_array, aux); */
-/* } */
+	arr_t aux;
+	aux.puntero_doble_array = &array;
+	aux.max = tamanio_array;
+
+	size_t tamanio_actual = 0;
+	aux.act = &tamanio_actual;
+
+	return abb_con_cada_elemento(arbol, recorrido, insertar_array, &aux);
+}
