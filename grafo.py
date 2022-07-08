766fb217c13b2b95917df1f6c832ec52e56c7980
lucho <lucianolorenzo1@hotmail.com>
2022-07-07 21:05:17 -0300

    grafo

--- grafo.py
+++ grafo.py
@@ -40,5 +40,37 @@ class Grafo:
         return cadena
 
 
-grafo = Grafo(5)
-print(grafo)
+class GrafoHH:
+    """
+    Grafo implementado con diccionarios de diccionarios
+    """
+    def __init__(self, nombresVertices):
+        self.vertices = dict()
+        for nombre in nombresVertices:
+            self.vertices[nombre] = {}
+
+    def insertar_arista(self, origen, destino, peso):
+        self.vertices[origen] = self.vertices.get(origen, {})
+        self.vertices[origen][destino] = peso
+
+    def insertar_arista_no_dirigida(self, v1, v2, peso):
+        self.vertices[v1] = self.vertices.get(v1, {})
+        self.vertices[v2] = self.vertices.get(v2, {})
+        self.vertices[v1][v2] = peso
+        self.vertices[v2][v1] = peso
+
+    def borrar_arista(self, origen, destino):
+        self.insertar_arista(origen, destino, inf)
+
+    def devuelve_distancia(self, origen: str, destino: str):
+        vertice = self.vertices.get(origen, {})
+        if destino in vertice:
+            return vertice[destino]
+        return inf
+
+    def obtener_adyacencias(self, origen: str) -> list:
+        vertice = self.vertices.get(origen, {})
+        return list(vertice)
+
+
+grafo = GrafoHH(["hospital", "colegio", "iglesia"])
